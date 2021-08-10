8cd2e1f979aca6be366942532193204b7a544690
VanshKela <vanshkela2@gmail.com>
2021-08-10 22:23:15 +0530

    Button made to submit

--- hub-collab/src/Screens/signup.js
+++ hub-collab/src/Screens/signup.js
@@ -71,7 +71,7 @@ class SignUp extends Component {
                 tech={this.state.tech}
               />
               {this.state.visibleButton ? (
-                <button onClick={event =>  window.location.href='/'} className="registerButton">Register</button>
+                <button type="submit" onClick={event =>  window.location.href='/'} className="registerButton">Register</button>
               ) : (
                 null
               )}
