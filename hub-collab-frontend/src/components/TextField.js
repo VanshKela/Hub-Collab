bd22322c7bd654c314b17f2d69c0f48be964fba5
VanshKela <vanshkela2@gmail.com>
2021-08-12 22:31:47 +0530

    Login validation added

--- hub-collab-frontend/src/components/TextField.js
+++ hub-collab-frontend/src/components/TextField.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./TextField.css";
 
-const TextField = ({ label, type, onChange, register }) => {
+const TextField = ({ label, type, onChange, register, properties,errors }) => {
   return (
     <div className="textfield">
       <h3> {label} </h3>
-      <input {...register(label, { required: true })} onChange={onChange} className="rounded-input" type={type}/>
+      <input {...register(label,properties)} onChange={onChange} className="rounded-input" type={type}/>
+      {errors && <p className="error">{errors.message}</p>}
     </div>
   );
 };
