fef4c56beeebaf86144852bb327a5da9aa082fee
VanshKela <vanshkela2@gmail.com>
2021-08-13 18:42:40 +0530

    Security added to api

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -2,7 +2,11 @@ const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signUp = catchAsync(async(req, res, next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        email: req.body.email,
+        password: req.body.password,
+        name: req.body.name
+    });
 
     res.status(201).json({
         status: 'success',
