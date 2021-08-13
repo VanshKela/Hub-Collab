67a21989de9a225de615c49a2c5e39b334f74d11
VanshKela <vanshkela2@gmail.com>
2021-08-13 11:41:00 +0530

    Database connected

---
+++ server/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const userRouter = require('./routes/userRoutes.js');
+
+const app = express();
+
+// 1) MIDDLEWARES
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+    console.log('Hello from the middleware 👋');
+    next();
+});
+
+app.use((req, res, next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+// 3) ROUTES
+// app.use('/api/v1/users', userRouter);
+
+module.exports = app;
